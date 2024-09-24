d99212bb9e6934eb0d29d015e19c0214c9dbb584
paulanelifo <162932861+paulanelifo@users.noreply.github.com>
2024-09-24 21:35:01 +0800

    Add files via upload

--- home/home.js
+++ home/home.js
@@ -1,78 +1,21 @@
-// ---------Responsive-navbar-active-animation-----------
-function test(){
-    var tabsNewAnim = $('#navbarSupportedContent');
-    var selectorNewAnim = $('#navbarSupportedContent').find('li').length;
-    var activeItemNewAnim = tabsNewAnim.find('.active');
-    var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
-    var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
-    var itemPosNewAnimTop = activeItemNewAnim.position();
-    var itemPosNewAnimLeft = activeItemNewAnim.position();
-    $(".hori-selector").css({
-      "top":itemPosNewAnimTop.top + "px", 
-      "left":itemPosNewAnimLeft.left + "px",
-      "height": activeWidthNewAnimHeight + "px",
-      "width": activeWidthNewAnimWidth + "px"
-    });
-    $("#navbarSupportedContent").on("click","li",function(e){
-      $('#navbarSupportedContent ul li').removeClass("active");
-      $(this).addClass('active');
-      var activeWidthNewAnimHeight = $(this).innerHeight();
-      var activeWidthNewAnimWidth = $(this).innerWidth();
-      var itemPosNewAnimTop = $(this).position();
-      var itemPosNewAnimLeft = $(this).position();
-      $(".hori-selector").css({
-        "top":itemPosNewAnimTop.top + "px", 
-        "left":itemPosNewAnimLeft.left + "px",
-        "height": activeWidthNewAnimHeight + "px",
-        "width": activeWidthNewAnimWidth + "px"
-      });
-    });
-  }
-  $(document).ready(function(){
-    setTimeout(function(){ test(); });
-  });
-  $(window).on('resize', function(){
-    setTimeout(function(){ test(); }, 500);
-  });
-  $(".navbar-toggler").click(function(){
-    $(".navbar-collapse").slideToggle(300);
-    setTimeout(function(){ test(); });
-  });
-  
-  
-  
-  // --------------add active class-on another-page move----------
-  jQuery(document).ready(function($){
-    // Get current path and find target link
-    var path = window.location.pathname.split("/").pop();
-  
-    // Account for home page with empty path
-    if ( path == '' ) {
-      path = 'index.html';
-    }
-  
-    var target = $('#navbarSupportedContent ul li a[href="'+path+'"]');
-    // Add active class to target link
-    target.parent().addClass('active');
-  });
-  
-  
-  
-  
-  // Add active class on another page linked
-  // ==========================================
-  // $(window).on('load',function () {
-  //     var current = location.pathname;
-  //     console.log(current);
-  //     $('#navbarSupportedContent ul li a').each(function(){
-  //         var $this = $(this);
-  //         // if the current path is like this link, make it active
-  //         if($this.attr('href').indexOf(current) !== -1){
-  //             $this.parent().addClass('active');
-  //             $this.parents('.menu-submenu').addClass('show-dropdown');
-  //             $this.parents('.menu-submenu').parent().addClass('active');
-  //         }else{
-  //             $this.parent().removeClass('active');
-  //         }
-  //     })
-  // });
+const loginButton = document.getElementById('login-button');
+const registerButton = document.getElementById('register-button');
+const popup = document.getElementById('popup');
+const overlay = document.getElementById('popup-overlay');
+
+// Open the pop-up when either button is clicked
+loginButton.addEventListener('click', () => {
+  popup.style.display = 'block';
+  overlay.style.display = 'block';
+});
+
+registerButton.addEventListener('click', () => {
+  popup.style.display = 'block';
+  overlay.style.display = 'block';
+});
+
+// Close the pop-up by clicking anywhere on the overlay
+overlay.addEventListener('click', () => {
+  popup.style.display = 'none';
+  overlay.style.display = 'none';
+});
